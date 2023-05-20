6a0d86c1d8cde45fa504e0e606efc595b7050785
Builderbot2000 <kevintang1118@outlook.com>
2023-05-20 16:45:27 -0700

    v5

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.static('public'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
