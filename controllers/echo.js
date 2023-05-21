9343c38958bde520492fd4239d2286517eace366
jaskaran27177 <jaskarandhanoa7@gmail.com>
2023-05-20 18:43:07 -0700

    new code

--- controllers/echo.js
+++ controllers/echo.js
@@ -1,9 +1,36 @@
 const fs = require('fs')
 const png = require("upng-js")
 const echoRouter = require('express').Router()
-
+const config=require('../utils/config')
 echoRouter.get('/', async(request, response)=>{
-    response.status(200).send('Hello')
+    var echoDB;
+    const iframestr='https://api.echo3D.com/webar?secKey=Izkby9ofQngS4y0HofpxZOAJ&key=wandering-tooth-7184&entry='
+    // Query echo3D
+    fetch('https://api.echo3D.com/query?secKey='+config.SEC_KEY+'&key=' + config.API_KEY)
+    .then((response) => response.json())
+    .then((json) => {
+    // Store database
+    response.status(200).send(Object.keys(json.db))
+    })
+    .catch((error) => {
+    console.error('here',error);
+    });
+    response.status(404).send('Notworking')
+})
+
+echoRouter.get(('/search/:name'),async(req,res)=>{
+    const name=req.params.name
+    const searchlink='https://api.echo3D.com/search?secKey=Izkby9ofQngS4y0HofpxZOAJ&key=wandering-tooth-7184&keywords='+name
+    const response= await fetch(searchlink)
+    console.log(response)
+    const found =response.find(pic=>{
+        if(pic.source==='poly'){
+            return pic
+        }
+    })
+    console.log(found)
+
+
 })
 
 echoRouter.get('/topo/:id', async(request, response)=>{
--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3003
 const MONGODB_URI = process.env.MONGODB_URI
-
+const API_KEY=process.env.API_KEY
+const SEC_KEY=process.env.SEC_KEY
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  API_KEY,
+  SEC_KEY
 }
